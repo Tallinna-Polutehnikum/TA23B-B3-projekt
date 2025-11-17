86698208db664a5f1123db14e5a3f5a798cfbd8d
Sofja <sofja.portnova@tptlive.ee>
2025-11-18 01:37:21 +0200

    making user port more secure

--- user-profile-site/vite.config.js
+++ user-profile-site/vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: 'localhost', 
-    port: 5158
+    host: true,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5158,
+    strictPort: false,
+    proxy: {
+      '/api': 'http://localhost:3000'
+    },
+    // https: ... (if you want to enable SSL)
   }
 })
